461ae2a61dfcc63d4260686d4b7311241ebba3a9
JessicaMoffatt <37711059+JessicaMoffatt@users.noreply.github.com>
2018-04-08 21:31:24 -0600

    second commit

---
+++ script.js
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Jessica Moffatt Resume</title>
+<link href="style.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+<div class="resume">
+<canvas id="logo_canvas" width="200" height="200"> 
+
+</canvas>
+<h1> JESSICA MOFFATT </h1>
+
+</div>
+
+</body>
+
+</html>

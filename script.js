c7fc7baaf51895606401dd3c0e3b6063e200274f
JessicaMoffatt <37711059+JessicaMoffatt@users.noreply.github.com>
2019-05-01 17:26:13 -0600

    website revamped
    now is a redirect to the new website

--- script.js
+++ script.js
@@ -1,32 +1,20 @@
-$(document).ready(function(){
-	$('#resume_link').hover(resumeIn, resumeOut);
-	$('#portfolio_link').hover(portfolioIn, portfolioOut);
-	$('#menu_img').click(function(){
-		if($('#menu_selection').is(":hidden")){
-			$('#menu_selection').fadeIn(100);
-		}else{
-			$('#menu_selection').fadeOut(100);
-		}	
-	});
-	$(document).click(function(e){
-		if($(e.target).closest('#menu_img').length === 0){
-			$('#menu_selection').fadeOut(100);
-		}
-	});
-	
-	function resumeIn(){
-		$('#main_img').attr("src","photos/main_resume.jpg")
-	}
-	
-	function resumeOut(){
-		$('#main_img').attr("src","photos/main.jpg");
-	}	
-	
-	function portfolioIn(){
-		$('#main_img').attr("src","photos/main_portfolio.jpg")
-	}
-	
-	function portfolioOut(){
-		$('#main_img').attr("src","photos/main.jpg");
-	}
-});
+window.onload = function(){
+
+(function(){
+  var countDown = 5;
+
+  setInterval(function() {
+    countDown--;
+    if (countDown >= 0) {
+      document.getElementById("count").innerHTML = countDown;
+    }
+    
+    if (counter === 0) {
+        clearInterval(counter);
+    }
+
+  }, 1000);
+
+})();
+
+}
